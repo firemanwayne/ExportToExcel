7434bedcb428460dd0c90dd65f4dd2184e10eb6a
Wayne Davis <wdavis2002@gmail.com>
2021-05-17 07:22:28 -0500

    Commit

--- src/Interfaces/IExcelDownloadService.cs
+++ src/Interfaces/IExcelDownloadService.cs
@@ -3,8 +3,16 @@ using System.Threading.Tasks;
 
 namespace Simple.ExportToExcel
 {
+    /// <summary>
+    /// Download exported excel File to the browser
+    /// </summary>
     internal interface IExcelDownloadService : IAsyncDisposable
     {
+        /// <summary>
+        /// Downloads the excel file to the browser
+        /// </summary>
+        /// <param name="Response"></param>
+        /// <returns></returns>
         ValueTask ExportFile(UploadResponse Response);
     }
 }
