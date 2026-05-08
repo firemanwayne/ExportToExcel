e3944376ad594b7952ce2cc99c943f66781215b1
wayne-davis_avionte <wayne.davis@simplevms.com>
2026-05-08 11:32:20 -0500

    update

--- samples/Simple.ExportToExcel.TestServer/Data/WeatherForecast.cs
+++ samples/Simple.ExportToExcel.TestServer/Data/WeatherForecast.cs
@@ -1,21 +1,20 @@
 using Simple.ExportToExcel.Attributes;
 using System;
 
-namespace Simple.ExportToExcel.TestServer.Data
+namespace Simple.ExportToExcel.TestServer.Data;
+
+[SpreadSheet("Weather Data", typeof(WeatherForecast))]
+public class WeatherForecast
 {
-    [SpreadSheet("Weather Data", typeof(WeatherForecast))]
-    public class WeatherForecast
-    {
-        [SpreadSheetColumn("Date", 0)]
-        public DateTime Date { get; set; }
+    [SpreadSheetColumn("Date", 0)]
+    public DateTime Date { get; set; }
 
-        [SpreadSheetColumn("Temp. Celsius", 1)]
-        public int TemperatureC { get; set; }
+    [SpreadSheetColumn("Temp. Celsius", 1)]
+    public int TemperatureC { get; set; }
 
-        [SpreadSheetColumn("Temp. Fahrenheit", 2)]
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    [SpreadSheetColumn("Temp. Fahrenheit", 2)]
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        [SpreadSheetColumn("Summary", 3)]
-        public string Summary { get; set; }
-    }
+    [SpreadSheetColumn("Summary", 3)]
+    public string Summary { get; set; }
 }
